cd3f9e1d43e39315f2c7642e4a339d8459da78ff
moiyad <moiyad117@gmail.com>
2018-03-12 17:04:24 +0300

    v3

--- app/src/main/java/com/example/pc/taskapplication/MainActivity.java
+++ app/src/main/java/com/example/pc/taskapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.pc.taskapplication;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,14 +16,20 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-//    ListView listView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        listView = (ListView) findViewById(R.id.listViewHeroes);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference base =database.getReference("base");
+        final DatabaseReference date =database.getReference("date");
+        final DatabaseReference rate =database.getReference("rates");
+
+
 
         Retrofit retrofit =new Retrofit.Builder()
                 .baseUrl(Api.BASE_URL)
@@ -39,16 +48,16 @@ public class MainActivity extends AppCompatActivity {
                 Example example = response.body();
                 Rates rates = response.body().getRates();
 
+                base.setValue(example.getBase());
+                date.setValue(example.getDate());
 
-                Log.d("rate AUD  ",String.valueOf(rates.getAUD()));
-                Log.d("rate BGN  ",String.valueOf(rates.getBGN()));
-                Log.d("rate BRL  ",String.valueOf(rates.getBRL()));
-                Log.d("rate CAD  ",String.valueOf(rates.getCAD()));
-                Log.d("rate CHF  ",String.valueOf(rates.getCHF()));
+                rate.child("AUD").setValue(rates.getAUD());
+                rate.child("BGN").setValue(rates.getBGN());
+                rate.child("BRL").setValue(rates.getBRL());
 
 
 
-//                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, ratesArr));
+
             }
 
             @Override
